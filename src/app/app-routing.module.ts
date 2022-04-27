38f3e2ceaff0d462d22a561001cff2b1df9166e2
camila corbalan <camincorbalan@gmail.com>
2022-04-26 21:28:05 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//aca se importan los componentes
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Aca van las rutas {path:'', component:pagina1 }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
